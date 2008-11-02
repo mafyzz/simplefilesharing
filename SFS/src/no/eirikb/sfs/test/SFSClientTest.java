04d90efdf119f38d3285b9522803f93fb5c9a1ca
eirikdb <eirikdb@6819d57a-a180-11dd-8ab5-df3da3b84efe>
2008-11-02 15:29:02 +0000

    Removed Netbeans red-error... Just to make Jon happy

--- SFS/src/no/eirikb/sfs/test/SFSClientTest.java
+++ SFS/src/no/eirikb/sfs/test/SFSClientTest.java
@@ -24,46 +24,47 @@ import no.eirikb.sfs.share.ShareUtility;
 public class SFSClientTest {
 
     public static void main(String[] args) throws Exception {
+        /*
         SFSServer server = new SFSServer(new SFSServerListener() {
-
-            public void createShareEvent(Share share) {
-                System.out.println("Crate sharrwe!");
-            }
+        
+        public void createShareEvent(Share share) {
+        System.out.println("Crate sharrwe!");
+        }
         }, 31338);
         int listenPort = (int) (Math.random() * (65536 - 1024) + 1024);
         System.out.println("Listen port: " + listenPort);
-
+        
         SFSClient client = new SFSClient(new SFSClientListener() {
-
-            public void addShare(Share share) {
-                System.out.println("Add Share!");
-            }
+        
+        public void addShare(Share share) {
+        System.out.println("Add Share!");
+        }
         }, "localhost", 31338, listenPort);
         // File file = new File("/users/eirikb/Desktop/Heroes.S03E06.HDTV.XviD-LOL.avi");
         File file = new File("/home/eirikb/test");
         Share share = ShareUtility.createShare(file);
         client.getLocalShares().put(share.getHash(), new LocalShare(file, share));
         client.getClient().sendObject(new CreateShareEvent(share));
-
+        
         SFSClient client2 = new SFSClient(new SFSClientListener() {
-
-            public void addShare(Share share) {
-                System.out.println("ADD SHARE");
-            }
+        
+        public void addShare(Share share) {
+        System.out.println("ADD SHARE");
+        }
         }, "localhost", 31338, listenPort + 1);
         
         Thread.sleep(1000);
         
         share = client2.getShares().get(0);
         client2.getClient().sendObject(new GetShareOwnersEvent(share));
-
+        
         Thread.sleep(10000);
         System.out.println("...");
         SFSClient client3 = new SFSClient(new SFSClientListener() {
-
-            public void addShare(Share share) {
-                System.out.println("Add another share!");
-            }
+        
+        public void addShare(Share share) {
+        System.out.println("Add another share!");
+        }
         }, "localhost", 31338, listenPort + 2);
         
         client3.getClient().sendObject(new GetSharesEvent());
@@ -72,6 +73,7 @@ public class SFSClientTest {
         
         share = client3.getShares().get(0);
         client3.getClient().sendObject(new GetShareOwnersEvent(share));
-
+        
+         */
     }
 }
