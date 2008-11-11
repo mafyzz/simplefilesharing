b638da1626ceff3be603d95fe9fa76111e497f72
eirikdb <eirikdb@6819d57a-a180-11dd-8ab5-df3da3b84efe>
2008-11-11 09:03:19 +0000

    New event - DownloadCompelteEvent, used to inform server that a client has a complete share

---
+++ SFS/src/no/eirikb/sfs/event/server/DownloadCompleteEvent.java
@@ -0,0 +1,57 @@
+/*
+ * =============================================================================
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <eirikb@google.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Eirik Brandtzæg
+ * =============================================================================
+ */
+package no.eirikb.sfs.event.server;
+
+import no.eirikb.sfs.client.Client;
+import no.eirikb.sfs.client.SFSClient;
+import no.eirikb.sfs.client.SFSClientListener;
+import no.eirikb.sfs.event.Event;
+import no.eirikb.sfs.server.Server;
+import no.eirikb.sfs.sfsserver.SFSServer;
+import no.eirikb.sfs.sfsserver.SFSServerListener;
+import no.eirikb.sfs.sfsserver.User;
+
+/**
+ *
+ * @author eirikb
+ * @author <a href="mailto:eirikb@google.com">eirikb@google.com</a>
+ */
+public class DownloadCompleteEvent extends Event {
+
+    private Integer shareHash;
+
+    public DownloadCompleteEvent(Integer shareHash) {
+        this.shareHash = shareHash;
+    }
+
+    public void execute(SFSServerListener listener, Server client, SFSServer server) {
+        User user = null;
+        for (User u : server.getUsers()) {
+            if (u.getServer().equals(client)) {
+                user = u;
+                break;
+            }
+        }
+        if (user != null) {
+            server.getShareHolders().get(shareHash).getUsers().add(user);
+        }
+    }
+
+    public void execute(SFSClientListener listener, SFSClient client) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void execute(SFSClientListener listener, SFSClient client, Server server) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void execute(SFSClientListener listener, SFSClient sfsClient, Client client) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
